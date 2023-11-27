fa1f542fd042c5ef68c1fc7e1e309fa0132fe921
Jenloke Magbojos <mjenloke@gmail.com>
2023-11-28 01:54:08 +0800

    implement basic vue routing

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
@@ -20,11 +20,44 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
-}
+}/
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
-</style>
+</style> -->
+
+<script setup>
+import { ref, computed } from 'vue';
+import Home from './components/Home.vue';
+import Calculator from './components/Calculator.vue';
+import Exercise from './components/Exercise.vue';
+import NotFound from './components/NotFound.vue';
+
+const routes = {
+  '/': Home,
+  '/about': Exercise,
+  '/calculator': Calculator
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
+</script>
+
+<template>
+  <a href="#/calculator">Calculator</a>
+  <a href="#/">Home</a>
+  <a href="#/exercise">About</a>
+  <!-- <a href="#/non-existent-path">Broken Link</a> -->
+  <component :is="currentView" />
+</template>
+
