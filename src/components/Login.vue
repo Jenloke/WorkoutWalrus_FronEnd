0293decb1c87660c85f846406722732b0d3ccbab
Pol Jalapan <poljalapan@gmail.com>
2023-11-28 06:09:53 +0800

    add login, register

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <header>
+        <toolbar>
+          <h1>Login</h1>
+        </toolbar>
+      </header>
+  
+      <main class="ion-padding">
+        <div>
+          <label for="username">Username</label>
+          <input id="username" type="text">
+        </div>
+  
+        <div>
+          <label for="password">Password</label>
+          <input id="password" type="password">
+        </div>
+  
+        <button>Login</button>
+      </main>
+    </div>
+  </template>
+
+<script setup>
+import { loginAuthentication } from '.\js\login.js'
+import { useRouter } from 'vue-router';
+const router = useRouter()
+router.isReady()
+const { username, password, authenticateUser } = loginAuthentication(router)
+</script>
---
+++ src/components/Register.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+      <header>
+        <toolbar>
+          <h1>Signup</h1>
+        </toolbar>
+      </header>
+  
+      <main class="ion-padding">
+        <div>
+          <label for="username">Username</label>
+          <input id="username" v-model="username" type="text">
+        </div>
+  
+        <div>
+          <label for="password">Password</label>
+          <input id="password" v-model="password" type="password">
+        </div>
+  
+        <div>
+          <label for="name">Full Name</label>
+          <input id="name" v-model="name" type="text">
+        </div>
+  
+        <div>
+          <label for="birthday">Birthday</label>
+          <input id="birthday" v-model="birthday" type="date">
+        </div>
+  
+        <div>
+          <label for="height">Height</label>
+          <input id="height" v-model="height" type="number">
+        </div>
+  
+        <div>
+          <label for="weight">Weight</label>
+          <input id="weight" v-model="weight" type="number">
+        </div>
+  
+        <button @click="signUp">Signup</button>
+      </main>
+    </div>
+  </template>
+
+<script setup>
+import { registerUser } from './js/register.js'
+import { useRouter } from 'vue-router';
+const router = useRouter()
+router.isReady()
+const {username, password, birthday, name, height, weight, signUp} = registerUser(router)
+</script>
