25a6d31fc658dcc9abadc2556b048d6c3a5a19c9
Jenloke Magbojos <mjenloke@gmail.com>
2023-11-28 05:20:24 +0800

    implement api call

--- src/components/Exercise.vue
+++ src/components/Exercise.vue
@@ -1,3 +1,18 @@
+<script>
+
+
+
+async function getExercise(url = urlExercise) {
+  const response = await fetch(url, {
+    headers: {
+      'X-Api-Key': 'H2hjF7GM2NnHzuZTm5Nakw==cnVmqdsMHbfV8EVb'
+    }
+  });
+  console.log(response.json());
+  // return response.json();
+}
+</script>
+
 <template>
     <h1>Exercise</h1>
 </template>
